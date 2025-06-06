720145dd42e505aee44cc471c65e67b0c208f128
Harika Atchi <harika.atchi@assurex.co>
2025-06-06 20:26:47 +0530

    UI for Note Component

---
+++ headapps/aa/src/components/NoteComponent.tsx
@@ -0,0 +1,16 @@
+/* eslint-disable react/no-unescaped-entities */
+
+export default function ResponsiveNote() {
+  return (
+    <div className="w-full text-center pt-[3.3rem] px-[7.5rem] pb-[1.4rem]">
+      <p className="text-3xl text-gray-700 py-[1.5rem] mx-auto">
+        <span className="block">
+          *That's what 10% of new AA customers pay for monthly vehicle breakdown cover.
+        </span>
+        <span className="block">
+          Source: survey of new business sales from theAA.com, 1 February 2025 to 30 April 2025.
+        </span>
+      </p>
+    </div>
+  );
+}
