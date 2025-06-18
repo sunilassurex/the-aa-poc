c645dd3ebda36bfa1e990205428a21a6a64d06ce
Khushi Mohile <khushi.mohile@assurex.co>
2025-06-18 10:18:19 +0000

    font changes in social links component

--- headapps/aa/src/components/SocialLinks.tsx
+++ headapps/aa/src/components/SocialLinks.tsx
@@ -9,8 +9,8 @@ export default function SocialLinks() {
 
   return (
     <div className="bg-[#f5f5f5] py-10 text-center">
-      <h2 className="text-[25px] md:text-[28px] font-light mb-4 text-black">Connect with us</h2>
-      <div className="flex justify-center gap-6">
+      <h2 className="text-[25px] md:text-[28px] !font-light mb-4 text-black">Connect with us</h2>
+      <div className="flex justify-center gap-8 md:!gap-8 xl:gap-6">
         {icons.map((icon, index) => (
           <div key={index} className="bg-[#1a1a1a] p-1 rounded-full hover:scale-105 transition">
             <Image
