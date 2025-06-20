3d767ddcaee4a24d1e919e3ca99b405d20378d81
Khushi Mohile <khushi.mohile@assurex.co>
2025-06-20 08:37:06 +0000

    added font family

--- headapps/aa/src/components/SocialLinks.tsx
+++ headapps/aa/src/components/SocialLinks.tsx
@@ -9,15 +9,15 @@ export default function SocialLinks() {
 
   return (
     <div className="bg-[#f5f5f5] py-10 text-center">
-      <h2 className="text-[25px] md:text-[28px] !font-light mb-4 text-black">Connect with us</h2>
+      <h3 className="text-[25px] md:!text-[28px] font-newtransport  !font-light mb-3 text-black">Connect with us</h3>
       <div className="flex justify-center gap-8 md:!gap-8 xl:gap-6">
         {icons.map((icon, index) => (
           <div key={index} className="bg-[#1a1a1a] p-1 rounded-full hover:scale-105 transition">
             <Image
               src={icon.src}
               alt={icon.alt}
-              width={45}
-              height={45}
+              width={47}
+              height={47}
               className="object-contain"
             />
           </div>
