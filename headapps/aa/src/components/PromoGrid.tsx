86fa507106605064029a0b22513e7df6d001d014
Harika Atchi <harika.atchi@assurexx.co>
2025-06-23 12:26:26 +0530

    UI fixes for promoSection

--- headapps/aa/src/components/PromoGrid.tsx
+++ headapps/aa/src/components/PromoGrid.tsx
@@ -18,14 +18,14 @@ export default function PromoGrid({ variant = 'cards' }: PromoGridProps) {
 
   if (variant === 'promo-section') {
     return (
-      <div className="bg-gray-100 space-y-10 px-[2rem] py-[1.5rem] md:!py-[1.5rem]">
-        <div className="wrapper">
+      <div className="bg-gray-100 space-y-10 py-[1rem] md:!py-[1.5rem]">
+        <div className="wrapper px-[1rem]">
           {/* Vixa Block */}
-          <div className="bg-white text-center md:max-w-[1160px] mx-auto pt-[4.2rem] px-[2.4rem] pb-[4rem] md:min-h-[300px] md:px-[12.6rem] md:pb-[6rem] md:pt-[5.2rem] shadow flex flex-col justify-center items-center">
-            <h2 className="md:!text-[2.4rem] md:mt-0 font-newtransport !font-bold md:!mb-[2rem]">
+          <div className="min-h-[391px] md:min-h-[391px] bg-white text-center  md:max-w-[1160px] mx-auto pt-[4.2rem] px-[2.4rem] pb-[4rem] md:min-h-[300px] md:px-[12.6rem] md:pb-[6rem] md:pt-[5.2rem] shadow flex flex-col justify-center items-center">
+            <h2 className="md:!text-[2.4rem] !text-[1.9rem] leading-[2.4rem] !mt-[22px] !mb-[1.3rem] md:mt-0 font-newtransport !font-bold md:!mb-[2rem]">
               Vixa from The AA
             </h2>
-            <p className="md:mb-[2.4rem] md:!text-[1.6rem] text-gray-700 font-newtransport !font-regular">
+            <p className="!font-normal text-[#1d1d1d]  md:mb-[2.4rem] md:!text-[1.6rem] !text-[1.8rem] !leading-[2.5rem] text-gray-700 font-newtransport !font-regular">
               Stay in the know and on the move with Vixa, our car care device and app.
               <br />
               <br />
@@ -36,32 +36,36 @@ export default function PromoGrid({ variant = 'cards' }: PromoGridProps) {
               <br />
               Personalised insights – reliable fixes – upfront pricing.
             </p>
-            <button className="shadow-custom-inset pt-[1.3rem] pb-[1.3rem] w-full md:w-auto  md:pt-[1.3rem] md:px-[2.7rem] md:pb-[1.5rem] !text-[1.8rem] md:!text-[1.8rem] bg-gradient-to-b from-[#ffcc00] to-[#f9b800]  text-black font-bold  rounded border border-[#d6a500]  shadow-[inset_0_2px_4px_rgba(255,255,255,0.2)]  transition duration-300  hover:from-[#ffe066] hover:to-[#ffd633] hover:border-[#ffd633]">
+            <button className="shadow-custom-inset md:w-auto w-full py-[0.9rem] px-[2rem] !text-[1.8rem] md:pt-[1.3rem] md:px-[2.7rem] md:pb-[1.5rem] md:!text-[1.8rem] bg-gradient-to-b from-[#ffcc00] to-[#f9b800]  text-black font-bold  rounded border border-[#d6a500] text-[1.1rem] shadow-[inset_0_2px_4px_rgba(255,255,255,0.2)]  transition duration-300  hover:from-[#ffe066] hover:to-[#ffd633] hover:border-[#ffd633]">
               Go to Vlxa
             </button>
           </div>
 
           {/* Car Lease Block */}
           <div
-            className="text-center md:mt-[2rem] md:max-w-[1160px] mx-auto pt-[4.2rem] px-[2.4rem] pb-[4rem] md:min-h-[300px] md:px-[12.6rem] md:pb-[6rem] md:pt-[5.2rem] shadow flex flex-col justify-center items-center bg-no-repeat bg-cover bg-[position:right_0]"
+            className="min-h-[391px] md:min-h-[415px] text-center mt-[2rem]  md:max-w-[1160px] mx-auto pt-[4.2rem] px-[2.4rem] pb-[4rem] md:min-h-[300px] md:px-[12.6rem] md:pb-[6rem] md:pt-[5.2rem] shadow flex flex-col justify-center md:items-center bg-no-repeat bg-cover bg-[position:right_0]"
             style={{ backgroundImage: "url('/large-beam.webp')" }}
           >
-            <h2 className="md:!text-[2.4rem] md:mt-0 font-newtransport !font-bold md:!mb-[2rem]">
+            <h2 className="md:!text-[2.4rem] md:mt-0 !text-[1.9rem] leading-[2.4rem] !mb-[1.3rem] font-newtransport !font-bold md:!mb-[2rem]">
               Looking for your next car?
             </h2>
-            <p className="md:mb-[2.4rem] md:!text-[1.6rem] text-gray-700 font-newtransport !font-regular">
+            <p className="md:mb-[2.4rem] !font-normal text-[#1d1d1d] md:!text-[1.6rem] tracking-[.025em] !text-[1.8rem] !leading-[2.5rem] text-gray-700 font-newtransport !font-regular">
               Interested in leasing a brand-new car with the latest tech? Prefer to buy a
               second-hand one with the confidence that it comes with free breakdown cover and a
               history check? With more than 100,000 cars to choose from, we’ve got you covered.
             </p>
-            <div className="flex justify-center gap-4 flex-wrap">
-              {buttons.map((btn, i) => (
+            <div
+              className={`flex ${
+                buttons.length === 1 ? 'justify-center' : 'justify-center gap-4 md:mx-[0.8rem]'
+              } flex-wrap`}
+            >
+              {buttons.map((btn, index) => (
                 <button
-                  key={i}
-                  className={`font-semibold rounded shadow w-full md:w-auto pt-[1.3rem] px-[2.7rem] pb-[1.5rem] md:pt-[1.3rem] md:px-[2.7rem] md:pb-[1.5rem] ${
+                  key={index}
+                  className={`font-semibold rounded shadow w-full py-[0.9rem] px-[2rem] !text-[1.8rem] md:w-auto md:pt-[1.3rem] md:px-[2.7rem] md:pb-[1.5rem] ${
                     btn.variant === 'primary'
-                      ? 'font-newtransport !font-bold !text-[1.8rem] w-full md:!text-[1.8rem] bg-gradient-to-b from-[#3d3d3d] to-[#161616]  text-white border-[1px] border-[#151515]  shadow-[inset_0_4px_4px_rgba(255,255,255,0.15)] transition hover:bg-[#5e5e5e]'
-                      : 'font-newtransport !font-bold text-black bg-transparent border-[0.2rem] border-[#1d1d1d] !text-[1.8rem] md:!text-[1.8rem] md:pt-[1.2rem] md:px-[3rem] md:pb-[1.4rem] hover:!bg-[#dcddde]'
+                      ? ' font-newtransport  !font-bold md:!text-[1.8rem] bg-gradient-to-b from-[#3d3d3d] to-[#161616]  text-white font-bold rounded  border-[1px] border-[#151515] text-[1.1rem]  shadow-[inset_0_4px_4px_rgba(255,255,255,0.15)]  transition  hover:bg-[#5e5e5e] hover:from-transparent hover:to-transparent'
+                      : 'font-newtransport !font-bold text-black bg-transparent rounded border-[0.2rem] border-[#1d1d1d] md:!text-[1.8rem] md:pt-[1.2rem] md:px-[3rem] md:pb-[1.4rem]  hover:!bg-[#dcddde]'
                   }`}
                 >
                   {btn.label}
