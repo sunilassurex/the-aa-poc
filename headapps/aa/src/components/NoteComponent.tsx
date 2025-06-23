8cdffc0e670f089ff0b8de9fbb1400f5721d0e25
Harika Atchi <harika.atchi@assurexx.co>
2025-06-23 13:11:27 +0530

    UI fixes for News component

--- headapps/aa/src/components/NoteComponent.tsx
+++ headapps/aa/src/components/NoteComponent.tsx
@@ -4,10 +4,10 @@ export default function Notes() {
   return (
     <div className="w-full text-center pt-[3.3rem] px-[7.5rem] pb-[1.4rem]">
       <p className="text-3xl text-gray-700 py-[1.5rem] mx-auto">
-        <span className="block">
+        <span className="block font-newtransport !font-regular">
           *That's what 10% of new AA customers pay for monthly vehicle breakdown cover.
         </span>
-        <span className="block">
+        <span className="block font-newtransport !font-regular">
           Source: survey of new business sales from theAA.com, 1 February 2025 to 30 April 2025.
         </span>
       </p>
