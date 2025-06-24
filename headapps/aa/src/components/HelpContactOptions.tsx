bac2610bd05e6772bb7a8ca922a15abec1b11259
Khushi Mohile <khushi.mohile@assurex.co>
2025-06-24 09:32:09 +0000

    font style changes

--- headapps/aa/src/components/HelpContactOptions.tsx
+++ headapps/aa/src/components/HelpContactOptions.tsx
@@ -15,27 +15,27 @@ export default function HelpContactOptions() {
   ];
 
   return (
-    <section className="text-center max-w-[1160px] mx-auto px-4 md:px-10 py-12">
-      <p className="text-xl tracking-wide text-gray-700 !mb-10 uppercase">Contact us</p>
+    <section className="text-center max-w-[1160px] mx-auto xl:!px-7 py-12">
+      <h1 className="text-xl tracking-wide text-gray-700 !mb-10">Contact us</h1>
 
-      <h1 className="text-3xl sm:text-5xl md:!text-6xl xl:!text-[55px] font-extrabold text-[#1a1a1a] !mb-8">
+      <h3 className="!text-6xl sm:!text-7xl md:!text-6xl xl:!text-[65px] !font-newtransport !font-bold text-[#1a1a1a] !mb-8">
         Find help or contact options
-      </h1>
+      </h3>
 
-      <p className="text-3xl  md:text-4xl xl:text-5xl font-light leading-relaxed text-gray-800 mb-16  mx-auto">
+      <h3 className="text-3xl md:text-4xl xl:text-5xl !font-newtransport !font-light leading-relaxed text-gray-800 mb-16 mx-auto">
         We’ve got online solutions, account log in, live chat and more – so you can complete your
         task or get support.
-      </p>
+      </h3>
 
-      <div className="flex flex-row md:flex-row justify-center items-start mt-17 gap-y-25 gap-x-15 md:gap-x-40 xl:gap-x-45">
+      <div className="flex flex-wrap justify-center items-start gap-10">
         {options.map((item, index) => (
-          <div key={index} className="w-fit flex flex-col items-center text-center">
+          <div key={index} className="w-36 sm:w-65 h-36 flex flex-col items-center text-center">
             <img
               src={item.img}
               alt={item.label}
-              className="w-20 h-20 sm:w-24 sm:h-24 object-contain"
+              className="w-20 h-20 sm:w-30 sm:h-30 object-contain mb-2"
             />
-            <span className="text-xl sm:text-xl md:text-2xl font-normal text-gray-900 mt-2 break-words">
+            <span className="text-[16px]  md:text-[18px] xl:text-[18px] font-normal text-gray-900 break-words leading-tight">
               {item.label}
             </span>
           </div>
