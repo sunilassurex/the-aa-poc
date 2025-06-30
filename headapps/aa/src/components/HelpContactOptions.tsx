4640fa85d79c8309c7ec48a1030564a67b0533ee
Khushi Mohile <khushi.mohile@assurex.co>
2025-06-30 06:49:40 +0000

    fix changes

--- headapps/aa/src/components/HelpContactOptions.tsx
+++ headapps/aa/src/components/HelpContactOptions.tsx
@@ -15,19 +15,19 @@ export default function HelpContactOptions() {
   ];
 
   return (
-    <section className="text-center max-w-[1160px] mx-auto xl:!px-7 py-12">
+    <section className="text-center max-w-[1160px] mx-auto  py-12">
       <h1 className="text-xl tracking-wide text-gray-700 !mb-10">Contact us</h1>
 
-      <h3 className="!text-6xl sm:!text-7xl md:!text-6xl xl:!text-[65px] !font-newtransport !font-bold text-[#1a1a1a] !mb-8">
+      <h3 className="!text-6xl sm:!text-7xl md:!text-6xl xl:!text-[64px] !font-newtransport !font-bold text-[#1a1a1a] !mb-8">
         Find help or contact options
       </h3>
 
-      <h3 className="text-3xl md:text-4xl xl:text-5xl !font-newtransport !font-light leading-relaxed text-gray-800 mb-16 mx-auto">
+      <h4 className="!text-[20px] pb-[15px] md:text-4xl md:px-0 xl:px-0 px-9 xl:!text-5xl !font-newtransport !font-light leading-relaxed text-gray-800 mb-16 mx-auto">
         We’ve got online solutions, account log in, live chat and more – so you can complete your
         task or get support.
-      </h3>
+      </h4>
 
-      <div className="flex flex-wrap justify-center items-start gap-10">
+      <div className="flex pt-[15px] flex-wrap justify-center items-start gap-10">
         {options.map((item, index) => (
           <div key={index} className="w-36 sm:w-65 h-36 flex flex-col items-center text-center">
             <img
