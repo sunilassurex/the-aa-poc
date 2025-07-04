338d8701e1a11c3e4b7fbc9f6008d66a8f845909
Khushi Mohile <khushi.mohile@assurex.co>
2025-07-04 12:08:21 +0000

    fixed

--- headapps/aa/src/components/PromoGrid.tsx
+++ headapps/aa/src/components/PromoGrid.tsx
@@ -23,7 +23,7 @@ export default function PromoGrid({ variant = 'cards' }: PromoGridProps) {
         <div className="wrapper !px-[1rem]">
           {/* Vixa Block */}
           <div className="md:!min-h-[390.6px] min-h-[479.6px] md:!pb-[1.5rem]">
-            <div className="  bg-white text-center  md:max-w-[1120px] mx-auto !pt-[3.9rem] !px-[2.4rem] !pb-[4rem]  md:!px-[12.6rem] md:!pb-[6.1rem] md:!pt-[5.2rem] flex flex-col justify-center items-center">
+            <div className="  bg-white text-center  md:max-w-[1120px] mx-auto !pt-[3.9rem] !px-[2.4rem] !pb-[4rem]  md:!px-[12.6rem] md:!pb-[6rem] md:!pt-[5rem] flex flex-col justify-center items-center">
               <h2 className="md:!text-[2.4rem] md:leading-[3.2rem] md:text-[#1d1d1d] md:!tracking-[-.025em] !text-[1.9rem] leading-[2.4rem] !mt-[25px] !mb-[1.3rem] md:mt-0 font-newtransport !font-bold md:!mb-[2.4rem]">
                 Vixa from The AA
               </h2>
@@ -130,9 +130,9 @@ export default function PromoGrid({ variant = 'cards' }: PromoGridProps) {
           {cards.map((card, index) => (
             <div
               key={index}
-              className={` relative w-full flex flex-col justify-center items-center text-black !pt-[4.1rem] px-[2.4rem] pb-[4rem] md:px-[4.4rem] md:pb-[6rem] md:pt-[5.27rem] min-h-[255px] md:min-h-[302.2px]  ${
+              className={` relative w-full flex flex-col justify-center items-center text-black !pt-[3.95rem] px-[2.4rem] pb-[4rem] md:px-[4.4rem] md:pb-[5.9rem] md:!pt-[5.36rem] min-h-[255px] md:min-h-[302.2px]  ${
                 card.backgroundImage ? 'bg-contain bg-no-repeat bg-center' : 'bg-white'
-              } ${index % 2 === 1 && index !== Array.length - 1 ? 'mb-[2.1rem] md:mb-0' : ''}
+              } ${index % 2 === 1 && index !== cards.length - 1 ? 'mb-[2.1rem] md:mb-0' : ''}
     `}
               style={
                 card.backgroundImage
@@ -147,18 +147,18 @@ export default function PromoGrid({ variant = 'cards' }: PromoGridProps) {
             >
               <a href={' '} className="block w-full">
                 <div>
-                  <h2 className="!mb-[1.25rem] !text-[1.9rem] leading-[2.4rem] tracking-[0] md:!text-[2.4rem] md:!leading-[3.2rem] md:!tracking-[-.025em] xl:!text-[2.4rem] font-newtransport !font-bold md:!my-[2rem] md:!mb-[2rem]">
+                  <h2 className="!mb-[1.3rem] !text-[1.9rem] leading-[2.49rem] tracking-[0] md:!text-[2.4rem] md:!leading-[3.2rem] md:!tracking-[-.025em] xl:!text-[2.4rem] font-newtransport !font-bold md:!my-[2rem] md:!mb-[2rem]">
                     {card.title}
                   </h2>
 
-                  <p className="font-newtransport !font-light !text-[1.8rem] !tracking-[.025em] text-[rgb(29, 29, 29)] !leading-[2.51rem] !mb-[2.4rem] !font-normal md:!leading-[2.25rem] md:!mb-[2.3rem] md:!text-[1.6rem] md:!tracking-[.025em] text-[#1d1d1d]">
+                  <p className="font-newtransport !font-light !text-[1.8rem] !tracking-[.025em] text-[rgb(29, 29, 29)] !leading-[2.51rem] !mb-[2.4rem] !font-normal md:!leading-[2.18rem] md:!mb-[2.44rem] md:!text-[1.6rem] md:!tracking-[.025em] text-[#1d1d1d]">
                     {card.description}
                   </p>
                 </div>
               </a>
               <div className="flex justify-center !w-full md:!w-auto">
                 <button
-                  className={`!w-full md:w-auto mx-auto font-bold !pt-[0.78rem] !pb-[0.9rem] md:pt-[1.35rem] md:px-[2.72rem] md:pb-[1.39rem] !rounded-[5px] transition !text-[1.85rem] md:!text-[1.8rem] leading-[.018em] md:leading-[2rem] md:tracking-[.029em]
+                  className={`!w-full md:w-auto mx-auto font-bold !pt-[0.85rem] !tracking-[0.015em] !pb-[0.8rem] md:!pt-[1.261rem] md:!px-[2.72rem] md:!pb-[1.491rem] !rounded-[5px] transition !text-[1.8rem] md:!text-[1.85rem] leading-[.018em] md:!leading-[2.05rem] md:tracking-[.029em]
     ${
       card.variant === 'primary'
         ? ` !w-full
