594b5e89c6a5028bfc5e1f8c3a577349769c2806
ksonali-mufin <k.sonali@mufin.co.kr>
2025-07-09 15:50:14 +0530

    toggle component fixes for mobile and desktop view

--- headapps/aa/src/components/Toggle.tsx
+++ headapps/aa/src/components/Toggle.tsx
@@ -28,18 +28,19 @@ export default function Toggle({ initiallyOpen = true }: ToggleProps) {
   };
 
   return (
+    <div className='bg-[#f6f6f6]'>
     <div className="w-full max-w-[1160px] mx-auto md:!px-[2rem] md:py-[1.5rem]" ref={containerRef}>
       <div
         onClick={handleToggle}
         role="button"
         tabIndex={0}
         onKeyDown={(e) => (e.key === 'Enter' || e.key === ' ') && handleToggle()}
-        className="cursor-pointer bg-white md:!mb-[1.5rem] md:!py-[1.4rem] md:!px-[9.5rem] !px-8 !py-5 flex justify-between items-center focus:outline-none shadow-[0_5px_10px_rgba(192,195,195,0.2)] h-[50px]"
+        className="cursor-pointer bg-white md:!mb-[1.5rem] md:!py-[1.4rem] md:!px-[9.5rem] !px-8 !py-5 flex justify-between items-center focus:outline-none shadow-[0_5px_10px_rgba(192,195,195,0.2)] h-[50px] md:w-[1070px] md:ml-[20px]"
       >
         {/* Left-aligned label */}
         <span
-          className={`block bg-white cursor-pointer relative mt-0 py-[1.4rem] text-[1.6rem] leading-[2.2rem] tracking-[0.025em] font-newtransport !font-bold h-[50px] ${
-            isActive ? 'underline decoration-1 underline-offset-[0.5px]' : ''
+          className={`block bg-white cursor-pointer relative mt-0 py-[1.4rem] text-[1.6rem] leading-[2.2rem] tracking-[0.025em] font-newtransport !font-bold h-[50px] !Arials ${
+            isActive ? 'underline decoration-[1px] decoration-black tracking-[0.3px] underline-offset-[1.5px]' : ''
           }`}
         >
           Good to know
@@ -59,9 +60,9 @@ export default function Toggle({ initiallyOpen = true }: ToggleProps) {
 
       {/* Content Box */}
       {isOpen && (
-        <div className="md:mb-[2rem]">
+        <div className="md:mb-[2rem] p-4">
           <div className="bg-white border border-gray-300 md:py-[3rem] md:px-[9.5rem] text-[14px] md:text-[16px] text-gray-800 ">
-            <div className="md:!mb-[0px] md:!text-[14px] md:!leading-[21px] ">
+            <div className="md:!mb-[0px] md:!text-[14px] md:!leading-[21px]  !py-[3rem] !px-[2.5rem]">
               {/* <sup>†</sup>
             Source: AA Annual Report &amp; Accounts 2024 (c. 2,800 patrols) and Annual Report &amp;
             Financial Statements 2023 (1,600 patrols). Verify at{' '}
@@ -129,6 +130,7 @@ export default function Toggle({ initiallyOpen = true }: ToggleProps) {
           </div>
         </div>
       )}
+    </div>
     </div>
   );
 }
