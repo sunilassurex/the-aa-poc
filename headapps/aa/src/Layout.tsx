dc2a37b7162ad0cff6c5ce1630b3d7c0e0da140d
Khushi Mohile <khushi.mohile@assurex.co>
2025-07-11 11:08:00 +0000

    layout chnages

--- headapps/aa/src/Layout.tsx
+++ headapps/aa/src/Layout.tsx
@@ -6,7 +6,6 @@ import Head from 'next/head';
 import { Placeholder, LayoutServiceData, Field, HTMLLink } from '@sitecore-jss/sitecore-jss-nextjs';
 import config from 'temp/config';
 import Scripts from 'src/Scripts';
-import Toggle from 'components/Toggle';
 
 // Prefix public assets with a public URL to enable compatibility with Sitecore Experience Editor.
 // If you're not supporting the Experience Editor, you can remove this.
@@ -38,7 +37,6 @@ const Layout = ({ layoutData, headLinks }: LayoutProps): JSX.Element => {
           <link rel={headLink.rel} key={headLink.href} href={headLink.href} />
         ))}
       </Head>
-<Toggle></Toggle>
       {/* root placeholder for the app, which we add components to using route data */}
       <div className={mainClassPageEditing}>
         <header>
