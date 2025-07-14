77f5aef37a297823fa753917490be708c334cf7d
Khushi Mohile <khushi.mohile@assurex.co>
2025-07-14 10:07:44 +0000

    help page fixes

--- headapps/aa/src/components/HelpContactOptions.tsx
+++ headapps/aa/src/components/HelpContactOptions.tsx
@@ -10,32 +10,47 @@ export default function HelpContactOptions() {
     },
     {
       img: '/chat-to-us.svg',
+      button: '',
       label: 'Chat to us',
     },
   ];
 
   return (
-    <section className="text-center max-w-[1160px] mx-auto  py-12">
-      <h1 className="text-xl tracking-wide text-gray-700 !mb-10">Contact us</h1>
+    <section className="text-center max-w-[1160px] mx-auto md:px-[2rem] px-[1rem]">
+      <div className="md:py-[1.4rem] py-[1rem] px-[2rem] md:px-0">
+        <h1 className="xl:!text-[1.39rem] !text-[1.2rem] !tracking-[0.05em] !font-light xl:!leading-[2.2rem] leading-[1.8rem] text-gray-700 md:pt-[1.9rem] pt-[1.5rem] md:!mb-[1.7rem] !mb-[1.6rem]">
+          Contact us
+        </h1>
 
-      <h3 className="!text-6xl sm:!text-7xl md:!text-6xl xl:!text-[64px] !font-newtransport !font-bold text-[#1a1a1a] !mb-8">
-        Find help or contact options
-      </h3>
+        <p className="!text-[3.6rem]  xl:!text-[6.4rem] !font-newtransport !font-bold xl:!leading-[7.4rem] !leading-[4.2rem] text-[#1a1a1a] !mb-[0.8rem] xl:!mb-[0.6rem]">
+          Find help or contact options
+        </p>
 
-      <h4 className="!text-[20px] pb-[15px] md:text-4xl md:px-0 xl:px-0 px-9 xl:!text-5xl !font-newtransport !font-light leading-relaxed text-gray-800 mb-16 mx-auto">
-        We’ve got online solutions, account log in, live chat and more – so you can complete your
-        task or get support.
-      </h4>
+        <h3 className="!text-[2rem]  md:!text-[1.9rem]  xl:!text-[2.6rem] tracking-[-0.005rem] !font-newtransport !font-light xl:!leading-[3.6rem] md:!leading-[2.4rem] !leading-[2.7rem] text-gray-800 xl:!mb-[1.4rem] !mb-[0.8rem] mx-auto">
+          We’ve got online solutions, account log in, live chat and more – so you can complete your
+          task or get support.
+        </h3>
+      </div>
 
-      <div className="flex pt-[15px] flex-wrap justify-center items-start gap-10">
+      <div className="flex md:py-[1.5rem] py-[1rem] flex-wrap justify-center items-start md:!gap-7 gap-0">
         {options.map((item, index) => (
-          <div key={index} className="w-36 sm:w-65 h-36 flex flex-col items-center text-center">
-            <img
-              src={item.img}
-              alt={item.label}
-              className="w-20 h-20 sm:w-30 sm:h-30 object-contain mb-2"
-            />
-            <span className="text-[16px]  md:text-[18px] xl:text-[18px] font-normal text-gray-900 break-words leading-tight">
+          <div key={index} className="md:!w-65 w-24 flex flex-col items-center text-center">
+            {item.img ? (
+              <img
+                src={item.img}
+                alt={item.label}
+                className="md:w-26 md:h-26 w-20 h-20 object-contain mb-2"
+              />
+            ) : item.button ? (
+              <button
+                type="button"
+                className="px-4 py-2 bg-[#ffcc00] text-black rounded-md  transition"
+              >
+                {item.button}
+              </button>
+            ) : null}
+
+            <span className="text-[14px] md:text-[18px] !font-newtransport !font-normal text-gray-900 break-words leading-[21.6px] mt-[4px]">
               {item.label}
             </span>
           </div>
