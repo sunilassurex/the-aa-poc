ebf9d7042323c0009ac45e56d08cc22c42590b62
Harika Atchi <harika.atchi@assurexx.co>
2025-08-04 12:09:30 +0530

    CardsWithOneTwoThree-Variant  Component

--- headapps/aa/src/components/CardsWithOneTwoThreeColumn.tsx
+++ headapps/aa/src/components/CardsWithOneTwoThreeColumn.tsx
@@ -9,6 +9,7 @@ type ContactItem = {
   cta: string;
   tabletMinHeight: string;
   desktopMinHeight: string;
+  variant?: 'emergency';
 };
 
 type Props = {
@@ -27,46 +28,89 @@ const CardsWithOneTwoThreeColumn: React.FC<Props> = ({ contacts }) => {
   useEffect(() => {
     setDeviceType(device);
   }, [device]);
-  //layout for 1 item
+
+  // layout for 1 item
   if (contacts.length === 1) {
     return (
       <div className="bg-[#f6f6f6] md:py-[1.5rem]">
         <div className="wrapper mx-auto !px-[2.5rem] md:!px-[1.1rem] lg:!px-[2rem]">
-          <div className="md:max-w-[106rem] md:mx-auto md:px-[2rem] grid">
-            {contacts.map((item, index) => (
-              <div
-                key={index}
-                className="bg-white block md:px-[1.1rem] lg:!px-[4rem] lg:!pt-[2.5rem] lg:!pb-[2.1rem] px-[2rem] pt-[2rem] pb-[1.1rem] md:!pt-[2.1rem] md:!pb-[2rem]"
-              >
-                <div className="mb-[2.2rem] md:mb-[0px]">
-                  <div className="">
-                    <div className="md:!float-left md:w-[33%] md:pr-[3rem] md:mr-[.4%]">
-                      <h5 className="lg:!mb-[0.4rem] md:!mb-[.6rem] lg:!text-[1.6rem] lg:!leading-[2.2rem] !leading-[2rem] !mb-[1.1rem] md:!pr-[0px] pr-[9rem] !tracking-[0px] md:!tracking-[.025em] md:!text-[15px]">
+          {contacts.map((item, index) => {
+            if (item.variant === 'emergency') {
+              return (
+                <div key={index} className="grid">
+                  <div className="flex flex-col items-center border-[8px] border-white bg-[#f6f6f6] px-[32px] py-[24px] gap-4 flex-[1_0_0] self-stretch shadow-[0px_0px_2px_0px_rgba(0,0,0,0.08),0px_1px_4px_0px_rgba(0,0,0,0.16)]">
+                    {/* Row: Icon + Heading */}
+                    <div className="flex items-baseline gap-[8px]">
+                      <img
+                        src="/warning-mark.png"
+                        alt="Warning Icon"
+                        className="w-[40px] h-[36px]"
+                      />
+                      <h2 className="!text-[24px] !leading-[32px] font-aa-sans !font-normal">
                         {item.title}
-                      </h5>
-                      <h3 className="lg:!mb-[0rem] !mb-[0.9rem] !text-[2rem] !leading-[2.7rem] !tracking-[-.005em] md:!leading-[2.4rem] md:!text-[1.9rem] lg:!text-[2.6rem] lg:!tracking-[-.005em] lg:!leading-[3.6rem]  md:!mb-[1.1rem]">
-                        {item.phone}
-                      </h3>
+                      </h2>
                     </div>
-                    <div className="w-auto !mb-[1.4rem] md:!mb-[0px] md:float-left md:w-[60%]">
-                      <p className="lg:!leading-[2.2rem] !leading-[2.2rem] md:!m-0 !text-[1.4rem] !mb-[1.4rem] !font-a !font-[Arial,Helvetica,Verdana,sans-serif] lg:!font-[NewTransport,sans-serif] lg:!text-[14px] md:!text-[13px] md:!leading-[1.9rem] md:!m-[0px]">
+
+                    {/* Column: Description + Phone */}
+                    <div className="flex flex-col items-center gap-2">
+                      <p className="text-[1.8rem] !leading-[26px] text-center !mb-[1rem]">
                         {item.description}
                       </p>
+                      <p className="!text-[1.8rem] !leading-[26px] !font-bold !mb-0">
+                        {item.phone}
+                      </p>
+                      <p></p>
+                      <div className="block w-full md:hidden">
+                        <a
+                          href="tel:03330046046"
+                          className=" button transparent-button"
+                          role="button"
+                        >
+                          Call now
+                        </a>
+                      </div>
                     </div>
-                    <div>
-                      <button
-                        className="block md:hidden text-[1.4rem] leading-[1.2rem] !min-w-[22rem] text-center bg-gradient-to-b from-[#ffcc00] to-[#f9b800] border border-[#ffc400] 
-                     shadow-[inset_0_1px_0_#fff0b3,0_0_10px_rgba(0,0,0,0.2)] text-[#1d1d1d] !text-[1.8rem] !tracking-[0.015em]  transform scale-100
-                     !rounded-[5px] !px-[2rem] !py-[0.9rem] mx-auto !mb-[1.4rem] !border-[0.1rem] !border-[#ffc400] !leading-[2rem]"
-                      >
-                        <a href={`tel:${item.phone.replace(/\s+/g, '')}`}>{item.cta}</a>
-                      </button>
+                  </div>
+                </div>
+              );
+            }
+
+            return (
+              <div key={index} className="md:max-w-[106rem] md:mx-auto md:px-[2rem] grid">
+                <div
+                  key={index}
+                  className="bg-white block md:px-[1.1rem] lg:!px-[4rem] lg:!pt-[2.5rem] lg:!pb-[2.1rem] px-[2rem] pt-[2rem] pb-[1.1rem] md:!pt-[2.1rem] md:!pb-[2rem]"
+                >
+                  <div className="mb-[2.2rem] md:mb-[0px]">
+                    <div className="">
+                      <div className="md:!float-left md:w-[33%] md:pr-[3rem] md:mr-[.4%]">
+                        <h5 className="lg:!mb-[0.4rem] md:!mb-[.6rem] lg:!text-[1.6rem] lg:!leading-[2.2rem] !leading-[2rem] !mb-[1.1rem] md:!pr-[0px] pr-[9rem] !tracking-[0px] md:!tracking-[.025em] md:!text-[15px]">
+                          {item.title}
+                        </h5>
+                        <h3 className="lg:!mb-[0rem] !mb-[0.9rem] !text-[2rem] !leading-[2.7rem] !tracking-[-.005em] md:!leading-[2.4rem] md:!text-[1.9rem] lg:!text-[2.6rem] lg:!tracking-[-.005em] lg:!leading-[3.6rem]  md:!mb-[1.1rem]">
+                          {item.phone}
+                        </h3>
+                      </div>
+                      <div className="w-auto !mb-[1.4rem] md:!mb-[0px] md:float-left md:w-[60%]">
+                        <p className="lg:!leading-[2.2rem] !leading-[2.2rem] md:!m-0 !text-[1.4rem] !mb-[1.4rem] !font-a !font-[Arial,Helvetica,Verdana,sans-serif] lg:!font-[NewTransport,sans-serif] lg:!text-[14px] md:!text-[13px] md:!leading-[1.9rem] md:!m-[0px]">
+                          {item.description}
+                        </p>
+                      </div>
+                      <div>
+                        <button
+                          className="block md:hidden text-[1.4rem] leading-[1.2rem] !min-w-[22rem] text-center bg-gradient-to-b from-[#ffcc00] to-[#f9b800] border border-[#ffc400] 
+                       shadow-[inset_0_1px_0_#fff0b3,0_0_10px_rgba(0,0,0,0.2)] text-[#1d1d1d] !text-[1.8rem] !tracking-[0.015em]  transform scale-100
+                       !rounded-[5px] !px-[2rem] !py-[0.9rem] mx-auto !mb-[1.4rem] !border-[0.1rem] !border-[#ffc400] !leading-[2rem]"
+                        >
+                          <a href={`tel:${item.phone.replace(/\s+/g, '')}`}>{item.cta}</a>
+                        </button>
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
-            ))}
-          </div>
+            );
+          })}
         </div>
       </div>
     );
@@ -84,9 +128,9 @@ const CardsWithOneTwoThreeColumn: React.FC<Props> = ({ contacts }) => {
                 style={{
                   boxShadow: '0 5px 10px rgba(192, 195, 195, 0.2)',
                   minHeight:
-                    deviceType == Constant.RESPONSIVE_VARIABLES.TABLET
+                    deviceType === Constant.RESPONSIVE_VARIABLES.TABLET
                       ? item.tabletMinHeight
-                      : deviceType == Constant.RESPONSIVE_VARIABLES.DESKTOP
+                      : deviceType === Constant.RESPONSIVE_VARIABLES.DESKTOP
                       ? item.desktopMinHeight
                       : '',
                 }}
