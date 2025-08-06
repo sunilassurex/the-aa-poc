fefc63eb4d0562307276d2436c818ba93d90a94b
Harika Atchi <harika.atchi@assurexx.co>
2025-08-06 13:31:25 +0530

    MultiCover Component

--- headapps/aa/src/components/MultiCoverComponent.tsx
+++ headapps/aa/src/components/MultiCoverComponent.tsx
@@ -1,8 +1,9 @@
 'use client';
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Constant from './helpers/Constant/Constant';
 import Buttons from './Buttons';
+import useResponsiveDevice from './helpers/CustomHelpers/ResponsiveHook';
 
 const services = [
   {
@@ -28,43 +29,77 @@ const services = [
 ];
 
 export default function MultiProduct() {
+  const device = useResponsiveDevice();
+  //set state for DeviceType to avoid hydration issue(will stay same rendering for all devices before useeffect)
+  const [deviceType, setDeviceType] = useState<string>(Constant.RESPONSIVE_VARIABLES.DESKTOP);
+  useEffect(() => {
+    setDeviceType(device);
+  }, [device]);
   return (
     <div className="row">
-      <div className="cf">
-        <div className="!pb-[2.5rem] !px-[0px] wrapper">
-          <h3 className="!mt-[2.9rem] !mb-[3.1rem] text-center">Text</h3>
+      <div className="px-0">
+        <div className="md:!pb-[2.5rem] !px-[0px] wrapper">
+          <h3 className="!my-[1.5rem] !text-[2rem] !leading-[2.7rem] !tracking-[-.005em] md:!mt-[2.9rem] md:!mb-[3.1rem] text-center md:!text-[2.6rem] md:!leading-[3.6rem] md:!tracking-[-0.005em]">
+            Text
+          </h3>
 
           {/* Mobile: block | Desktop: table */}
-          <div className="block lg:table w-full lg:table-fixed">
-            <div className="block lg:table-row">
+          <div className="block md:table w-full md:table-fixed">
+            <div className="block md:table-row">
               {services.map((item, index) => (
                 <div
                   key={index}
-                  className="block lg:table-cell align-top w-full px-[0.5rem] multi-product"
+                  className={`block md:table-cell align-top w-full md:px-[0.5rem] ${
+                    index === 0 ? '' : 'pt-[1.4rem] md:!pt-[0px]'
+                  }`}
                 >
                   <figure className="block w-full !m-0 !p-0 !border-b-0"></figure>
                   <div
-                    className="bg-white !pt-[2.3rem] !px-[3rem] !pb-[9.3rem] border-b border-l border-r border-[#dfdfe0] relative"
-                    style={{ height: '263px' }}
+                    className="!py-[2rem] !pr-[2rem] !pb-[7.6rem] !pl-[2rem] bg-white md:!pt-[2.3rem] md:!px-[3rem] md:!pb-[9.3rem] border-b border-l border-r border-[#dfdfe0] relative"
+                    style={{
+                      height:
+                        deviceType === Constant.RESPONSIVE_VARIABLES.MOBILE
+                          ? ''
+                          : deviceType === Constant.RESPONSIVE_VARIABLES.TABLET
+                          ? '286px'
+                          : '263px',
+                    }}
                   >
                     <hgroup>
-                      <h2 className="alt !mb-[0.8rem] text-center">{item.title}</h2>
-                      <h6 className="!mb-[40px] !tracking-[-.05rem] text-center">
+                      <h2 className="alt !mb-[0.8rem] text-center !text-[2rem] !leading-[2.5rem] !tracking-[0px] !mb-[0.8rem]">
+                        {item.title}
+                      </h2>
+                      <h6 className="!mb-[40px] !tracking-[-.05rem] text-center !text-[1.8rem] !leading-[2.5rem] !tracking-[0.0025em] !mb-[3rem]">
                         {item.description}
                       </h6>
                     </hgroup>
                     <div className="absolute bottom-[25px] left-0 w-full">
-                      <Buttons
-                        buttons={[
-                          {
-                            href: item.link,
-                            title: item.label,
-                            buttonText: item.label,
-                            buttonType: Constant.BUTTONS.PRIMARY,
-                            buttonTextAlign: 'text-center',
-                          },
-                        ]}
-                      />
+                      {deviceType === Constant.RESPONSIVE_VARIABLES.MOBILE ? (
+                        <Buttons
+                          buttons={[
+                            {
+                              href: item.link,
+                              title: item.label,
+                              buttonText: item.label,
+                              buttonType: Constant.BUTTONS.PRIMARY,
+                              buttonTextAlign: 'text-center',
+                              buttonWidth: 'w-full',
+                            },
+                          ]}
+                        />
+                      ) : (
+                        <Buttons
+                          buttons={[
+                            {
+                              href: item.link,
+                              title: item.label,
+                              buttonText: item.label,
+                              buttonType: Constant.BUTTONS.PRIMARY,
+                              buttonTextAlign: 'text-center',
+                            },
+                          ]}
+                        />
+                      )}
                     </div>
                   </div>
                 </div>
