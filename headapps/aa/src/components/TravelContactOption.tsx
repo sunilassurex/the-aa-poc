1b79e30d7f1f96a7498bf58f7f0408aec1204fad
Khushi Mohile <khushi.mohile@assurex.co>
2025-08-08 06:32:33 +0000

    responsive img rendring

--- headapps/aa/src/components/TravelContactOption.tsx
+++ headapps/aa/src/components/TravelContactOption.tsx
@@ -3,17 +3,25 @@
 import Image from 'next/image';
 import Buttons from './Buttons';
 import Constant from './helpers/Constant/Constant';
+import useResponsiveDevice from './helpers/CustomHelpers/ResponsiveHook';
 
 export default function ContactHelp() {
+  const deviceType = useResponsiveDevice();
   return (
     <div className="md:py-[1.5rem] py-[1rem]">
       <div className="xl:max-w-[1160px] md:max-w-[74.6rem] mx-auto xl:px-[2rem] px-[1.1rem]">
         <div className="flex flex-col md:flex-row w-full items-start md:items-center justify-between bg-white rounded-[15px] shadow-sm p-[24px] md:pt-[40px] md:pb-[25px] md:px-[80px]">
           {/* Mobile Image (top-left only on small screens) */}
-          <div className="block md:hidden mb-[24px]">
-            <Image src="/help-support-sign-posting.svg" alt="Contact Icon" width={48} height={48} />
-          </div>
-
+          {deviceType === Constant.RESPONSIVE_VARIABLES.MOBILE && (
+            <div className="block md:hidden mb-[24px]">
+              <Image
+                src="/help-support-sign-posting.svg"
+                alt="Contact Icon"
+                width={48}
+                height={48}
+              />
+            </div>
+          )}
           <div className="flex-1 md:pr-4">
             <h2 className="!text-[22px] md:!text-[24px] md:!leading-[20px] !leading-[24px] font-aa-sans !font-bold text-black !mb-[16px]">
               Find help or contact options
