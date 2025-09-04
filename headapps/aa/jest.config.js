ee776d14a5162ca287e0cd7f8ff776a282d2797b
SUNIL\sunil <sunil.sharma1@tothenew.com>
2025-09-04 15:17:55 +0530

    added test

--- headapps/aa/jest.config.js
+++ headapps/aa/jest.config.js
@@ -1,9 +1,9 @@
-/** @type {import('jest').Config} */
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   collectCoverage: true,
   coverageDirectory: 'coverage',
   coverageReporters: ['lcov'],
-  testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'],
+  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+  // If your test files are somewhere else, update paths accordingly
 };
